2e18fb84e397bdce2db2ae7deef7be26f09ad603
tawangimjiz <s6501012630115@email.kmutnb.ac.th>
2025-10-28 03:01:20 +0700

    Fix case-sensitive import for Vercel deployment

--- mai_bit_kub_website/src/main.jsx
+++ mai_bit_kub_website/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
-import App from './App.jsx'
+import App from './app.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
